4a07214055d8eed7901fefc5cac1b85d48c408d6
Shepherd <mavhernandez@mcl.edu.ph>
2023-08-22 11:41:55 -0700

    Update Function02.cs
    TEST CASE 08232023

--- FunctionApp1/Function02.cs
+++ FunctionApp1/Function02.cs
@@ -26,7 +26,7 @@ namespace FunctionApp1
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? "TA POC TEST CASE 08232023"
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
